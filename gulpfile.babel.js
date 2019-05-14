e6ab2ca6fd8bc8f6369fe0bb8ce2af8c1c890965
Santiago Gil <gil.educaciontdf@gmail.com>
2019-05-14 01:28:15 -0300

    Probando fix precache

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -100,7 +100,7 @@ function buildSite(cb, options) {
 gulp.task("generate-service-worker", () => {
     return workbox.generateSW({
         cacheId: "chuncane",
-        globDirectory: "./dist",
+        globDirectory: "./",
         globPatterns: [
             "**/*.{css,js,eot,ttf,woff,woff2,otf}"
         ],

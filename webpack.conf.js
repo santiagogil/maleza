0dff9b2151664f03236d6bd10c28284a1d3902aa
Santiago Gil <gil.educaciontdf@gmail.com>
2019-05-15 02:00:51 -0300

    Intenta precache via webpack

--- webpack.conf.js
+++ webpack.conf.js
@@ -1,5 +1,7 @@
 import webpack from "webpack";
 import path from "path";
+const {GenerateSW} = require('workbox-webpack-plugin');
+
 
 export default {
   module: {
@@ -20,6 +22,71 @@ export default {
   plugins: [
     new webpack.ProvidePlugin({
       "fetch": "imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch"
+    }),
+    new GenerateSW({
+      swDest: 'sw.js',
+      include: [/\.css$/, /\.js$/],
+      skipWaiting: true,
+      clientsClaim: true,
+
+        runtimeCaching: [
+            {
+                urlPattern: /(?:\/)$/,
+                handler: "staleWhileRevalidate",
+                options: {
+                    cacheName: "html",
+                    expiration: {
+                        maxAgeSeconds: 60 * 60 * 24 * 7,
+                    },
+                },
+            },
+            {
+                urlPattern: /\.(?:png|jpg|jpeg|gif|bmp|webp|svg|ico)$/,
+                handler: "cacheFirst",
+                options: {
+                    cacheName: "images",
+                    expiration: {
+                        maxEntries: 1000,
+                        maxAgeSeconds: 60 * 60 * 24 * 365,
+                    },
+                },
+            },
+            {
+                urlPattern: /\.(?:mp3|wav|m4a)$/,
+                handler: "cacheFirst",
+                options: {
+                    cacheName: "audio",
+                    expiration: {
+                        maxEntries: 1000,
+                        maxAgeSeconds: 60 * 60 * 24 * 365,
+                    },
+                },
+            },
+            {
+                urlPattern: /\.(?:m4v|mpg|avi)$/,
+                handler: "cacheFirst",
+                options: {
+                    cacheName: "videos",
+                    expiration: {
+                        maxEntries: 1000,
+                        maxAgeSeconds: 60 * 60 * 24 * 365,
+                    },
+                },
+            },
+            {
+                urlPattern: /\.(?:woff|woff2)$/,
+                handler: "cacheFirst",
+                options: {
+                    cacheName: "fonts",
+                    expiration: {
+                        maxEntries: 1000,
+                        maxAgeSeconds: 60 * 60 * 24 * 365,
+                    },
+                },
+            }
+
+        ],
+
     })
   ],
 

19c96283522ff3a4f69b9d328a107f023d6ecac3
Santiago Gil <gil.educaciontdf@gmail.com>
2019-08-07 18:57:57 -0300

    Limpia js

--- src/js/app.js
+++ src/js/app.js
@@ -1,45 +1,12 @@
 // JS Goes here - ES6 supported
 //javascript file
-var lozad = require('lozad')
 
-      const observer = lozad()
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-  window.addEventListener('load', () => {
-      observer.observe()
+  gtag('config', 'UA-141188337-2');
 
-      let sidebar = document.getElementById('sidebar')
-      let hamburger = document.getElementById('hamburger')
-      let close = document.getElementById('close')
-
-  hamburger.addEventListener('click', event => {
-      event.preventDefault()
-      sidebar.classList.toggle('transform-off')
-  })
-
-  close.addEventListener('click', event => {
-      event.preventDefault()
-      hamburger.style.transform = ''
-      sidebar.classList.toggle('transform-off')
-  })
-    let share = document.getElementById('web-share')
-    let url = window.location.href
-    let title = document.title
-    if (share) {
-      if (!navigator.share) return share.remove()
-      share.addEventListener('click', event => {
-        // event.preventDefault()
-        if (!navigator.share) return share.remove()
-        navigator.share({"url": url, "title": title})
-          .then(console.log)
-          .catch(console.log)
-      })
-    }
-  // window.dataLayer = window.dataLayer || [];
-  // function gtag(){dataLayer.push(arguments);}
-  // gtag('js', new Date());
-
-  // gtag('config', 'UA-141188337-1');
-})
 // if (window.netlifyIdentity) {
 //   window.netlifyIdentity.on("init", user => {
 //     if (!user) {
